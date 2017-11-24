61098270c404b9e132559e73bc9eca86860d9f6f
helenewekre <hewe16@student.cbs.dk>
2017-11-24 16:44:26 +0100

    Problems with checking answers in question.js

--- js/question.js
+++ js/question.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
 
 
 
-            questions.forEach( (question)=> {
+            questions.forEach((question)=> {
                 SDK.Storage.persist('questionID', question.questionId);
 
                 $questionList.append(`
@@ -14,7 +14,7 @@ $(document).ready(() => {
                     <div class="col-lg-8">
                       <dl>
                         <dt> </dt>
-                        <dd>${question.question}
+                        <dd>Question (${question.questionId}): ${question.question}
                         <div id="option-list${question.questionId}"></div>
                         </dd>
                         
@@ -23,23 +23,56 @@ $(document).ready(() => {
                 </div>
             `);
 
-            SDK.Quiz.loadOptions( (e, options)=>{
+            SDK.Quiz.loadOptions((e, options)=> {
                 if (e) throw e;
                 const $optionList = $('#option-list' + question.questionId);
-                options.forEach( (option)=>{
+                options.forEach((option)=> {
                     $optionList.append(`
                         <input type="radio" name="options${question.questionId}" value="option${option.optionId}"> ${option.option}<br>
                     `);
                     }
-                )
-                }
-            );
-
-            SDK.Storage.remove('questionID');
+                );
+                    $('#submitBtn').click(() => {
+                        clickHandler(question, options);
+                    });
             }
-            );
 
+
+            );
+            SDK.Storage.remove('questionID');
+            });
+
+            /*
+            $('#submitBtn').click(() => {
+               console.log("");
+            });
+            */
+
+        });
+
+    function clickHandler(question, options) {
+        var correct = 0;
+        var correctQuestions = [];
+        var wrong = 0;
+
+        for (var i = 0; i < options.length; i++) {
+            wrong ++;
+            if (options[i].checked==true) {
+                if (options[i].isCorrect === 1) {
+                    correct ++;
+                    correctQuestions.push(question.questionId);
+                } else if (options[i].isCorrect === 0) {
+                    wrong --;
+                }
+            }
         }
-        );
-    }
-)
+        window.alert('You got ' + correct + ' correct answers, and ' + wrong + ' wrong answers!' +
+            'Question ' + correctQuestions.toString() + 'are correct. ');
+
+        console.log(correct);
+        console.log(wrong);
+
+    };
+
+
+});
